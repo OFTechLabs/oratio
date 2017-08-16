8cc4febe925f0212f4f084d8ad7aefb8128c2c95
jelleverstegen <jelle.verstegen@gmail.com>
2017-08-16 15:45:52 +0200

    cleaned webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -6,78 +6,10 @@ var fs = require('fs');
 var os = require('os');
 var dts = require('dts-bundle');
 var deleteEmpty = require('delete-empty');
-var LoaderOptionsPlugin = require("webpack/lib/LoaderOptionsPlugin");
 
 const name = 'oratio';
 
-/* helper function to get into build directory */
-var libPath = function (name) {
-    if (undefined === name) {
-        return 'dist';
-    }
-
-    return path.join('dist', name);
-};
-
-/* helper to clean leftovers */
-var outputCleanup = function (dir, initial) {
-    if (false == fs.existsSync(libPath())) {
-        return;
-    }
-
-    if (true == initial) {
-        console.log("Build leftover found, cleans it up.");
-    }
-
-    var list = fs.readdirSync(dir);
-    for (var i = 0; i < list.length; i++) {
-        var filename = path.join(dir, list[i]);
-        var stat = fs.statSync(filename);
-
-        if (filename == "." || filename == "..") {
-            // pass these files
-        } else if (stat.isDirectory()) {
-            // outputCleanup recursively
-            outputCleanup(filename, false);
-        } else {
-            // rm fiilename
-            fs.unlinkSync(filename);
-        }
-    }
-    fs.rmdirSync(dir);
-};
-
-/* precentage handler is used to hook build start and ending */
-var percentage_handler = function handler(percentage, msg) {
-    if (0 == percentage) {
-        /* Build Started */
-        outputCleanup(libPath(), true);
-        console.log("Build started... Good luck!");
-    } else if (1 == percentage) {
-        // TODO: No Error detection. :(
-        create_browser_version(webpack_opts.output.filename);
-
-        // Invokes dts bundling
-        console.log("Bundling d.ts files ...");
-        dts.bundle(bundle_opts);
-        add_module_oratio_to_typings()
-
-        // Invokes lib/ cleanup
-        deleteEmpty(bundle_opts.baseDir, function (err, deleted) {
-            if (err) {
-                console.error("Couldn't clean up : " + err);
-                throw err;
-            } else {
-                console.log("Cleanup " + deleted);
-            }
-        });
-    }
-};
-
-var bundle_opts = {
-
-    // Required
-
+const typingBundleOptions = {
     name: name,
     main: 'src/main.d.ts',
     baseDir: 'src',
@@ -87,16 +19,40 @@ var bundle_opts = {
     exclude: /^defs\/$/,
     removeSource: true,
     newline: os.EOL,
-    indent: '	',
-    prefix: '__',
-    separator: '/',
     verbose: false,
     emitOnIncludedFileNotFound: false,
     emitOnNoIncludedFileNotFound: false,
     outputAsModuleFolder: true
 };
 
-var webpack_opts = {
+const libPath = (name) => {
+    if (name === undefined) {
+        return 'dist';
+    }
+
+    return path.join('dist', name);
+};
+
+const percentageHandler = function handler(percentage) {
+    if (percentage === 0) {
+        outputCleanup(libPath(), true);
+        console.log("Build started... Good luck!");
+    } else if (percentage === 1) {
+        createBrowserVersion(webpackOptions.output.filename);
+
+        dts.bundle(typingBundleOptions);
+        addModuleNameToTypings()
+
+        deleteEmpty(typingBundleOptions.baseDir, function (err, deleted) {
+            if (err) {
+                console.error("Couldn't clean up : " + err);
+                throw err;
+            }
+        });
+    }
+};
+
+const webpackOptions = {
     entry: './src/main.ts',
     target: 'node',
     output: {
@@ -136,7 +92,7 @@ var webpack_opts = {
     externals: [nodeExternals()],
     plugins: [
         new webpack.optimize.UglifyJsPlugin(),
-        new webpack.ProgressPlugin(percentage_handler),
+        new webpack.ProgressPlugin(percentageHandler),
         new webpack.LoaderOptionsPlugin({
             options: {
                 tslint: {
@@ -150,13 +106,33 @@ var webpack_opts = {
     ]
 };
 
-var create_browser_version = function (inputJs) {
+const outputCleanup = (dir, initial) => {
+    if (!fs.existsSync(libPath())) {
+        return;
+    }
+
+    const list = fs.readdirSync(dir);
+    for (var i = 0; i < list.length; i++) {
+        const filename = path.join(dir, list[i]);
+        const stat = fs.statSync(filename);
+
+        if (filename === "." || filename === "..") {
+        } else if (stat.isDirectory()) {
+            outputCleanup(filename, false);
+        } else {
+            fs.unlinkSync(filename);
+        }
+    }
+    fs.rmdirSync(dir);
+};
+
+
+const createBrowserVersion = (inputJs) => {
     let outputName = inputJs.replace(/\.[^/.]+$/, "");
     outputName = `${outputName}.browser.js`;
-    console.log("Creating browser version ...");
 
     let b = browserify(inputJs, {
-        standalone: bundle_opts.name,
+        standalone: typingBundleOptions.name,
     });
 
     b.bundle(function (err, src) {
@@ -167,23 +143,21 @@ var create_browser_version = function (inputJs) {
     }).pipe(fs.createWriteStream(outputName));
 };
 
-var add_module_oratio_to_typings = function () {
+const addModuleNameToTypings = function () {
     fs.readFile("./dist/index.d.ts", (err, data) => {
         let dataString = data.toString();
-        dataString = dataString.replace(/.*\n/, "declare namespace " + name +"  {\n")
+        dataString = dataString.replace(/.*\n/, "declare namespace " + name + "  {\n")
         dataString += "}\n\n"
         dataString += "declare module \"" + name + "\" {\n"
-        dataString += "\texport = "  + name + ";\n"
+        dataString += "\texport = " + name + ";\n"
         dataString += "}\n"
         fs.writeFile("./dist/index.d.ts", dataString, (err, data) => {
             if (err) {
                 console.error("Couldn't add namespace to .d.ts: " + err);
                 throw err;
-            } else {
-                console.log("Added namespace to .d.ts");
             }
         });
     });
 }
 
-module.exports = webpack_opts;
+module.exports = webpackOptions;
